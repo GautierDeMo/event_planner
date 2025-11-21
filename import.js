7fcb993d42485457c36c4aa63053193b2b7bc9cf
Gautier de MAUROY <89637837+GautierDeMo@users.noreply.github.com>
2025-11-21 11:14:55 +0100

    🔧 refactor: format code for consistency
    - 🎨 align indentation for better readability in import functions
    - 🎨 improve consistency in console log formatting for data import messages

--- import.js
+++ import.js
@@ -4,26 +4,26 @@ import truegisterData from "./data/truegister.json" with {type: "json"};
 import {disisfine, liveticket, truegister} from "./main.js";
 
 async function addLiveticket() {
-    await liveticket.insertOne(liveticketData);
+  await liveticket.insertOne(liveticketData);
 }
 
 async function addTruegister() {
-    await truegister.insertOne(truegisterData);
+  await truegister.insertOne(truegisterData);
 }
 
 async function addDisisfine() {
-    await disisfine.insertOne(disisfineData);
+  await disisfine.insertOne(disisfineData);
 }
 
 export async function runImport() {
-    await addLiveticket();
-    console.log('‍🧑🏼💻 Liveticket data imported !');
-    console.log('');
-    await addTruegister();
-    console.log('🧑🏼💻 Truegister data imported !');
-    console.log('');
-    await addDisisfine();
-    console.log('🏠🔥🐶☕️ Disisfine data imported !');
-    console.log("Rapport au meme t'sais");
-    console.log('');
+  await addLiveticket();
+  console.log('‍🧑🏼💻 Liveticket data imported !');
+  console.log('');
+  await addTruegister();
+  console.log('🧑🏼💻 Truegister data imported !');
+  console.log('');
+  await addDisisfine();
+  console.log('🏠🔥🐶☕️ Disisfine data imported !');
+  console.log("Rapport au meme t'sais");
+  console.log('');
 }
--- main.js
+++ main.js
@@ -16,36 +16,28 @@ export const liveticket = database.collection("liveticket");
 // find all documents from each collection and map them
 async function allDisisfine() {
     const allDatas = await disisfine.find({}).toArray();
-    console.log('--- DISISFINE ---');
     for (const disisfine of allDatas) {
         const mappedDisisfine = mapEvent('disisfine', disisfine);
         // ajouter l'export dans mysql
         mappedDisisfine.attendees = mapAttendees('disisfine', disisfine._id, disisfine);
-        // console.log(mappedDisisfine);
     }
-    console.log('');
 }
 
 async function allTruegister() {
     const allDatas = await truegister.find({}).toArray();
-    console.log('--- TRUEGISTER ---');
     for (const truegister of allDatas) {
         const mappedTruegister = mapEvent('truegister', truegister);
         // ajouter l'export dans mysql
         mappedTruegister.attendees = mapAttendees('truegister', truegister._id, truegister);
-        // console.log(mappedTruegister);
     }
-    console.log('');
 }
 
 async function allLiveticket() {
     const allDatas = await liveticket.find({}).toArray();
-    console.log('--- LIVETICKET ---');
     for (const liveticket of allDatas) {
         const mappedLiveticket = mapEvent('liveticket', liveticket);
         // ajouter l'export dans mysql
         mappedLiveticket.attendees = mapAttendees('liveticket', liveticket._id, liveticket);
-        // console.log(mappedLiveticket);
     }
 }
 
